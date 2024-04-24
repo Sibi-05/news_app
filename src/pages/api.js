2a36f5d0df883724b03cf69b0cd9c4fede6dd68c
sibi <ssibi769@gmail.com>
2024-04-24 12:43:56 +0530

    second change

--- src/pages/api.js
+++ src/pages/api.js
@@ -1,11 +1,11 @@
-const api_key = process.env.REACT_APP_NEWS_API_KEY;
+const api_key = "884527771134448e91f839f12442934e";
 export const fetchNews = async (category) => {
   try {
     const res = await fetch(
       `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=${api_key}`
     );
     const data = await res.json();
-
+    console.log(api_key);
     return data;
   } catch (error) {
     console.error("Error fetching news sources:", error);
